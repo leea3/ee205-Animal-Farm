2e3f07084ad892d4d1e8b49028413e0a322f4a5f
leea3 <leea3@hawaii.edu>
2022-04-21 09:50:10 -1000

    Stubbed out Node class

---
+++ Node.h
@@ -0,0 +1,16 @@
+///////////////////////////////////////////////////////////////////////////////
+///         University of Hawaii, College of Engineering
+/// @brief  ee205_animal_farm - EE 205 - Spr 2022
+///
+/// @file Node.h
+/// @version 1.0
+///
+/// @author Arthur Lee <leea3@hawaii.edu>
+/// @date   21_Apr_2022
+///////////////////////////////////////////////////////////////////////////////
+#pragma once
+
+class Node {
+
+};
+
