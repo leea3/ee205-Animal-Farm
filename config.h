9ee74df0afe3b6c58c59f33b1baa21371c9da2c8
leea3 <leea3@hawaii.edu>
2022-04-21 09:15:28 -1000

    Added PRINT_HEADING_FOR_DUMP and FORMAT_LINE_FOR_DUMP macros

--- config.h
+++ config.h
@@ -15,3 +15,19 @@
 #define MAX_NAME1 "12345678901234567890123456789012345678901234567890"
 #define ILLEGAL_NAME "123456789012345678901234567890123456789012345678901"
 #define PROGRAM_NAME "Animal Farm 2"
+
+/// Format the heading for dumping members of a class to the console
+///
+/// Print =====================
+#define PRINT_HEADING_FOR_DUMP \
+    /* Print =========================================================== */ \
+    std::cout << std::setw(80) << std::setfill( '=' ) << "" << std::endl
+/// Format a line for dumping the members of a class to the console.
+/// Setup the fields for printing (space pad, left justify, etc.)
+#define FORMAT_LINE_FOR_DUMP( className, member ) \
+    std::cout << std::setfill( ' ' ) /* Space pad */ \
+              << std::left /* Left justify */ \
+              << std::boolalpha /* Print `true` or `false` for `bool`s */ \
+              << std::setw(8) << (className) \
+              << std::setw(20) << (member) \
+              << std::setw(52) /* (data) */
